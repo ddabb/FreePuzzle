6ea970161e74c86d2e6bd58f50c0830efbe3b25e
ddabb <417144452@qq.com>
2020-12-13 10:57:18 +0800

    hello free puzzle

---
+++ FreePuzzle.Api/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreePuzzle.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpGet]
+        public String Get()
+        {
+      
+            return "Hello, Free Puzzle";
+        }
+    }
+}
--- FreePuzzle/IFreePuzzle.cs
+++ FreePuzzle/IFreePuzzle.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace FreePuzzle
 {
+    /// <summary>
+    /// 所有谜题都实现该接口
+    /// </summary>
     public interface IFreePuzzle
     {
 
