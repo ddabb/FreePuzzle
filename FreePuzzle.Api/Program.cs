1f4b805e48fd1dbc91bbd2b4b4254adc41386024
ddabb <417144452@qq.com>
2020-12-20 23:41:24 +0800

    fix       AutofacServiceProviderFactory

--- FreePuzzle.Api/Program.cs
+++ FreePuzzle.Api/Program.cs
@@ -1,3 +1,4 @@
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -9,12 +10,13 @@ namespace FreePuzzlie.Api
         {
             CreateHostBuilder(args).Build().Run();
         }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                    .UseStartup<Startup>();
                 });
     }
 }
