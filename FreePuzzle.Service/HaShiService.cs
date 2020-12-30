319b14a5732543a2ea66e55db432b1c814bfe8c5
ddabb <417144452@qq.com>
2020-12-30 12:03:38 +0800

    待补充映射逻辑

--- FreePuzzle.Service/HaShiService.cs
+++ FreePuzzle.Service/HaShiService.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-
+using System.Linq;
 namespace FreePuzzle.Service
 {
     public class HaShiService : FreePuzzleServiceBase
@@ -46,8 +46,24 @@ namespace FreePuzzle.Service
                 new Node(77,3),
                 new Node(80,2),
             };
+            foreach (var item in nodeList)
+            {
+                item.SetAdjacentIndex(nodeList);
+            }
 
             return nodeList;
+        }
+
+    }
+
+    public class IndexMap
+    {
+        public int StartIndex;
+        public int EndIndex;
+        public IndexMap (int startIndex,int endIndex)
+        {
+            StartIndex = startIndex;
+            EndIndex = endIndex;
         }
 
     }
@@ -57,10 +73,95 @@ namespace FreePuzzle.Service
         public int Index { get; set; }
         public int Value { get; set; }
 
-        public Node(int index, int value)
+        public int Width = 9;
+
+        public int TopIndex { get; set; }
+
+        public int LeftIndex { get; set; }
+
+        public int BottomIndex { get; set; }
+
+        public int RightIndex { get; set; }
+
+        public List<IndexMap>  IndexMaps
+        {
+            get
+            {
+                List<IndexMap> map = new List<IndexMap>();
+                if (TopIndex!=0)
+                {
+                    map.Add(new IndexMap(Index, TopIndex));
+                }
+                if (LeftIndex!=0)
+                {
+                    map.Add(new IndexMap(Index, LeftIndex));
+                }
+                if (RightIndex != 0)
+                {
+                    map.Add(new IndexMap(Index, RightIndex));
+                }
+                if (BottomIndex != 0)
+                {
+                    map.Add(new IndexMap(Index, BottomIndex));
+                }
+                return map;
+            }
+
+        }
+
+        public Node(int index, int value, int width = 9)
         {
             this.Index = index;
             this.Value = value;
+        }
+
+        public int Row
+        {
+            get
+            {
+                if (Index % 9 == 0)
+                    return Index / 9;
+                else
+                {
+                    return (Index / 9) + 1;
+                }
+            }
+
+        }
+
+        public void SetAdjacentIndex(List<Node> nodes)
+        {
+            var otherNode = nodes.Where(c => c.Index != Index).ToList();
+            if (otherNode.Exists(c => c.Column == this.Column && c.Row < this.Row))
+            {
+                TopIndex = otherNode.Where(c => c.Column == this.Column && c.Row < this.Row).OrderBy(c => c.Index).Last().Index;
+            }
+            if (otherNode.Exists(c => c.Column == this.Column && c.Row > this.Row))
+            {
+                BottomIndex = otherNode.Where(c => c.Column == this.Column && c.Row > this.Row).OrderBy(c => c.Index).First().Index;
+            }
+            if (otherNode.Exists(c => c.Row == this.Row && c.Column < this.Column))
+            {
+                LeftIndex = otherNode.Where(c => c.Row == this.Row && c.Column < this.Column).OrderBy(c => c.Index).Last().Index;
+            }
+            if (otherNode.Exists(c => c.Row == this.Row && c.Column > this.Column))
+            {
+                RightIndex = otherNode.Where(c => c.Row == this.Row && c.Column > this.Column).OrderBy(c => c.Index).First().Index;
+            }
+        }
+
+        public int Column
+        {
+            get
+            {
+                if (Index % 9 == 0)
+                    return 9;
+                else
+                {
+                    return Index % 9;
+                }
+            }
+
         }
     }
 }
--- FreePuzzle.UnitTest/HaShiServiceTest.cs
+++ FreePuzzle.UnitTest/HaShiServiceTest.cs
@@ -4,7 +4,7 @@ using Xunit.Abstractions;
 using System.Linq;
 namespace FreePuzzle.UnitTest
 {
-    public class HaShiServiceTest :TestBase
+    public class HaShiServiceTest : TestBase
     {
         public HaShiServiceTest(ITestOutputHelper testOutput) : base(testOutput)
         {
@@ -15,7 +15,19 @@ namespace FreePuzzle.UnitTest
         {
             var list = IniType<HaShiService>().Solve();
             Assert.Equal(36, list.Count);
-            Assert.Equal(88, list.Sum(c=>c.Value));
+            Assert.Equal(88, list.Sum(c => c.Value));
+            foreach (var item in list)
+            {
+                Output.WriteLine($"Index {item.Index} Row {item.Row} Column {item.Column} TopIndex {item.TopIndex}  LeftIndex {item.LeftIndex}  RightIndex {item.RightIndex}  BottomIndex {item.BottomIndex}");
+            }
+
+            //var temp = list.SelectMany(c => c.IndexMaps).ToList();
+            //Output.WriteLine($"组合可能数{temp.Count}");
+            //foreach (var item in temp)
+            //{
+            //    Output.WriteLine($"item.StartIndex {item.StartIndex}  item.EndIndex{item.EndIndex}");
+            //}
+     
         }
     }
 }
